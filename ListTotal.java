a9ba00004ba1c3790124f41237f1c9eee15b72fc
Kamden Wright <600022173@fjuhsd.org>
2021-02-01 23:05:43 +0000

    Added code to add total sum

--- ListTotal.java
+++ ListTotal.java
@@ -1,15 +1,31 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kamden Wright
 //Date -
 
 import java.util.List;
 import java.util.ArrayList;
 
+
+
 public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
- }
+   //return 0;
+    int sum = 0; 
+    for(int i = 0; i < ray.size(); i++){
+      sum += ray.get(i); 
+      return sum;
+    }
+  }
 }
+
+ 
+  
+ 
+
+
+
+
+
